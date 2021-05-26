e78b4ce1415cfe976fdf485aec74c719e4cb6b38
Miguel Sierra <miguel109737@gmail.com>
2021-05-26 14:23:12 -0500

    Organizado el proyecto

--- pag/DCN.py
+++ pag/DCN.py
@@ -1,40 +1,43 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect
 
-app = Flask(__name__)
+from pag import app
 
-#en las rutas se usa el redender para redirigir a una pagina html
+#en las rutas se usa el redender para redirigir pag una pagina html
 @app.route("/")
-
 @app.route("/inicio")
 def inicio():
     return render_template("PPrincipal.html")
+
 
 @app.route ("/sedes")
 def sedes():
     return render_template("sedes.html", title="Sedes") #Se pasa el html y el titulo de la pagina
 
+
 @app.route ("/contacto")
 def contacto():
     return render_template("contacto.html", title="Contacto")
+
 
 @app.route ("/servicios")
 def servicios():
     return render_template("servicios.html", title="Servicios")
 
+
 @app.route ("/ferias")
 def ferias():
     return render_template("ferias.html", title="Ferias")
 
+
 @app.route ("/proveedores")
 def proveedores():
     return render_template("proveedores.html", title="Proveedores")
+
+
 @app.route ("/administrador")
 def administrador():
     return render_template("administrador.html", title="Administrador")
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
 
 #Aún pueden faltar rutas y definir bien las existentes. Tenemos que crear la forma de el token único de los proveedores,
 #averiguar la forma de definir calendarios en la aplicación y definir una template HTML
