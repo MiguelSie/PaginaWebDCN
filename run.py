c849d2051ef2b54fee5dab6d0ea0b0c4fe35510a
Miguel Sierra <miguel109737@gmail.com>
2021-06-01 13:10:06 -0500

    Preparado para despliegue

--- run.py
+++ run.py
@@ -3,6 +3,4 @@ from paginawebdcn import app
 if __name__ == '__main__':
     app.run(debug=True)
 
-# Aún pueden faltar rutas y definir bien las existentes. Tenemos que crear la forma de el token único de los proveedores,
-# averiguar la forma de definir calendarios en la aplicación y definir una template HTML
-# Módulo admin
+
